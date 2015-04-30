fceb693d0d3382338472ee296048e46983abda72
zhshijie <438218114@qq.com>
2015-04-30 10:03:04 +0800

    Initial Commit

---
+++ LifeIsland/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LifeIsland
+//
+//  Created by 张世杰 on 15/4/30.
+//  Copyright (c) 2015年 张世杰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
